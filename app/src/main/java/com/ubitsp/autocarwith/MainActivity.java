13efa9c669c57434e228a7f39e416fce7d037771
jiajunouyang <ubitsp@outlook.com>
2024-05-10 09:55:46 +0800

    initial

---
+++ app/src/main/java/com/ubitsp/autocarwith/MainActivity.java
@@ -0,0 +1,27 @@
+package com.ubitsp.autocarwith;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        Intent intent=new Intent();
+        intent.setClassName("com.miui.carlink","com.carwith.launcher.settings.phone.UCarScreenSettingsActivity");
+        startActivity(intent);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
